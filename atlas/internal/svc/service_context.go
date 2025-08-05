f8556ea9f2979e52a18486e277f5f60650a27d58
ZhangXiaoliang <zxl2024@qq.com>
2025-08-05 17:33:49 +0800

    暴露user服务的端口

--- atlas/internal/svc/service_context.go
+++ atlas/internal/svc/service_context.go
@@ -2,14 +2,23 @@ package svc
 
 import (
 	"douyin-mall/atlas/internal/config"
+	"douyin-mall/user/userclient"
+
+	"github.com/zeromicro/go-zero/zrpc"
 )
 
 type ServiceContext struct {
-	Config config.Config
+	Config  config.Config
+	UserRpc userclient.User
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
+
+	var userRpc userclient.User
+	userRpc = userclient.NewUser(zrpc.MustNewClient(c.UserRpc))
+
 	return &ServiceContext{
-		Config: c,
+		Config:  c,
+		UserRpc: userRpc,
 	}
 }
