4c11e553d92f94e11f95f902d898c444343398ee
ZhangXiaoliang <zxl2024@qq.com>
2025-08-05 20:51:41 +0800

    更新用户RPC客户端引用路径

--- atlas/internal/svc/service_context.go
+++ atlas/internal/svc/service_context.go
@@ -2,20 +2,20 @@ package svc
 
 import (
 	"douyin-mall/atlas/internal/config"
-	"douyin-mall/user/userclient"
+	"douyin-mall/user/user_client"
 
 	"github.com/zeromicro/go-zero/zrpc"
 )
 
 type ServiceContext struct {
 	Config  config.Config
-	UserRpc userclient.User
+	UserRpc user_client.User
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 
-	var userRpc userclient.User
-	userRpc = userclient.NewUser(zrpc.MustNewClient(c.UserRpc))
+	var userRpc user_client.User
+	userRpc = user_client.NewUser(zrpc.MustNewClient(c.UserRpc))
 
 	return &ServiceContext{
 		Config:  c,
